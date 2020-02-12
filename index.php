fdca4734c079876d9fb9fa78fa4035765a32030e
Hasan Çiçek <badboy714@gmail.com>
2020-02-12 20:48:56 +0300

    Sayfalama değiştirildi.

--- index.php
+++ index.php
@@ -9,80 +9,26 @@
   if (!isset($sayfa) OR $sayfa == 'anasayfa') {
     include 'inc/inc.carousel.php'; 
   }
-  
 
-  switch ($sayfa) {
-    case 'anasayfa':
-      include 'anasayfa.php';
-      break;
+  $arrSayfalar = array();
+  $arrSayfalar[] = 'anasayfa';
+  $arrSayfalar[] = 'hakkimizda';
+  $arrSayfalar[] = 'hizmetler';
+  $arrSayfalar[] = 'iletisim';
+  $arrSayfalar[] = '1-kolonlu-sayfa';
+  $arrSayfalar[] = '2-kolonlu-sayfa';
+  $arrSayfalar[] = '3-kolonlu-sayfa';
+  $arrSayfalar[] = '4-kolonlu-sayfa';
+  $arrSayfalar[] = 'tek-sayfa';
+  $arrSayfalar[] = '1.-blog-anasayfa';
+  $arrSayfalar[] = '2.-blog-anasayfa';
+  $arrSayfalar[] = 'ilet-sayfasi';
+  $arrSayfalar[] = 'genis-sayfa';
+  $arrSayfalar[] = 'kenar-cubugu';
+  $arrSayfalar[] = 'sik-sorulan-sorular';
+  $arrSayfalar[] = '404';
+  $arrSayfalar[] = 'fiyatlandirma-tablosu';
 
-    case 'hakkimizda':
-      include 'hakkimizda.php';
-      break;
-
-    case 'hizmetler':
-      include 'hizmetler.php';
-      break;
-
-    case 'iletisim':
-      include 'iletisim.php';
-      break;
-
-    case '1-kolonlu-sayfa':
-      include '1-kolonlu-sayfa.php';
-      break;
-
-    case '2-kolonlu-sayfa':
-      include '2-kolonlu-sayfa.php';
-      break;
-
-    case '3-kolonlu-sayfa':
-      include '3-kolonlu-sayfa.php';
-      break;
-
-    case '4-kolonlu-sayfa':
-      include '4-kolonlu-sayfa.php';
-      break;
-
-    case 'tek-sayfa':
-      include 'tek-sayfa.php';
-      break;
-
-    case '1.-blog-anasayfa':
-      include '1.-blog-anasayfa.php';
-      break;
-
-    case '2.-blog-anasayfa':
-      include '2.-blog-anasayfa.php';
-      break;
-
-    case 'ilet-sayfasi':
-      include 'ilet-sayfasi.php';
-      break;
-
-    case 'genis-sayfa':
-      include 'genis-sayfa.php';
-      break;
-
-    case 'kenar-cubugu':
-      include 'kenar-cubugu.php';
-      break;
-
-    case 'sik-sorulan-sorular':
-      include 'sik-sorulan-sorular.php';
-      break;
-
-    case '404':
-      include '404.php';
-      break;
-
-    case 'fiyatlandirma-tablosu':
-      include 'fiyatlandirma-tablosu.php';
-      break;
-    
-    default:
-      include 'anasayfa.php';
-      break;
-  }
-
+  if( in_array($sayfa, $arrSayfalar)) $Yukle = $sayfa;
+  include $Yukle . '.php';
   include('inc/inc.footer.php'); 
