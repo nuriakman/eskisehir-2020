556d06b7b1a29a567e907edd4b6c1b6fbc719221
Hasan Çiçek <badboy714@gmail.com>
2020-02-12 17:50:21 +0300

    Hakkımızda sayfası düzenlendi.

--- hakkimizda.php
+++ hakkimizda.php
@@ -21,13 +21,6 @@
         <h2>Eskişehir 2020 Tema Hakkında</h2>
         <p>Bu tema <b>"Özgür Yazılım Kış Kampı Eskişehir 2020 PHP ve MySQL ile Web Programlamaya Giriş"</b> sınıfının <b>Birlikte Öğrenelim</b> projesi için Modern Business temasının uyarlanmış ve modifiye edilmiş halidir.</p>
         <p>Original tema MIT lisanlı olup, bu haliyle isteyen istediği şekilde kullanabilir.Her hangi bir yerinde tema adından ya da bizlerden bahsetmesine gerek yoktur. Yine de Modern Business Tema adından söz ederse iyi olur.</p>
-        <p><b>Temaya kattığımız bazı özellikler;</b></p>
-        <ul>
-          <li>Tema parçalanarak php dosyaları ve dizin yapısı düzenlenmiştir,</li>
-          <li>Url koruması, sayfa başlarında yer alan "kontrol" değişkeni ve .htaccess dosyası ile güvenilir duruma getirilmiştir,</li>
-          <li>Sayfalama url adresleri seo() fonksiyonu ile seo uyumlu hale getirilmiştir,</li>
-          <li>Anasayfa'nın tamamı ve hakkımızda sayfasının bir bölümü dışında kalan sayfalar Modern Business teması orjinalliğinde bırakılmıştır.</li>
-        </ul>
       </div>
     </div>
     <!-- /.row -->
